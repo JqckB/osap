cbc9fc4dce395e3b66493b00eb4316850794a00b
maigre_a <dewep.net@gmail.com>
2014-03-29 12:10:23 +0100

    Creation page login + CSS + Term

--- www/cgi-bin/login.pl
+++ www/cgi-bin/login.pl
@@ -5,7 +5,8 @@ use CGI::Carp;
 use Socket;
 use IO::Handle;
 use Net::DNS;
-
+use DBI;
+use Digest::SHA1  qw(sha1);
 use Sys::Syslog qw(:standard :extended);
 
 #----------------------------------------------------------------------
@@ -31,11 +32,11 @@ sub login_window
 	print $q->start_table({-width=>'100%'},{-border=>'0'});
 	my $register_widget = $q->table({-border=>'0'},
 		$q->Tr($q->td({-colspan=>2, -align=>LEFT}, 
-		'Bienvenu sur la page de connexion.<br/>Pour acceder au reseau internet, merci d\'utiliser vos identifiants Datlan :')),
+		'Bienvenue sur la page de connexion.<br/>Pour acceder au reseau internet, merci d\'utiliser vos identifiants Datlan :')),
 		$q->Tr($q->td({-align=>RIGHT}, 'Utilisateur :'),
 			$q->td($q->textfield({-size=>50, name=>'name'}))),
 		$q->Tr($q->td({-align=>RIGHT}, 'Mot de passe:'),
-			$q->td($q->textfield({-size=>50, name=>'email', type=>'password'}))));
+			$q->td($q->password_field({-size=>50, name=>'password'}))));
 	print $q->Tr($q->td({align=>CENTER}, $register_widget));
 	my $read_terms="J'ai lu et j'accepte ";
 	my $terms_link=$q->a({-href=>'/osap/terms.html'}, 'les conditions d\'utilisations');
@@ -72,21 +73,11 @@ sub session_window {
 	print $q->Tr($q->td({align=>CENTER}, $q->img({src=>'/osap/logo.png'})));
 	print $q->end_table, "\n";
 	print $q->start_form;
-	print $q->h2({align=>CENTER}, "You are connected to  the LAAS network.");
-	print $q->h3({align=>CENTER}, "Please keep this window open.");
-	print $q->p("&nbsp;Once you want to disconnect from the network, ",
-	    "click on the button below:");
-	print $q->p("You can add this page to your bookmarks to come ",
-		   "back here later.");
-	print $q->p("If you are a member of the LAAS and are using this ",
-		"machine to connect on a regular basis, you should register ",
-		"it with sysadmin.");
-	print $q->p("If your machine is already registered, you should ",
-		"use the WIFI network id that was given to you ",
-		"instead of ", $q->b("laas-welcome"), ".");
+	print $q->h3({align=>CENTER}, "Connexion r&eacute;ussie.");
+	print $q->p("Une fois que vous souhaiterez vous d&eacute;connecter du r&eacute;seau, vous pouvez cliquer sur le bouton suivant :");
 	print $q->start_table({width=>'100%'});
 	print $q->Tr({align=>CENTER},
-		     $q->td($q->submit({-name=>'OK', -value=>'Disconnect',
+		     $q->td($q->submit({-name=>'OK', -value=>'Deconnexion',
 					-class=>'oswap-button-cancel'})));
 	print $q->end_table;
 	print $q->end_form;
@@ -100,45 +91,41 @@ sub disconnected_window {
 	my $q = $_[0];
 	my $myself = $q->self_url;
 	
-	print $q->header, $q->start_html(-title=>'Disconnected',
-					 -style=>{-src=>'/osap/osap.css'});
+	print $q->header, $q->start_html(-title=>'OSAP Login', -style=>{-src=>'/osap/osap.css'});
+	
+	print $q->h1('Datlan Connexion');
+	print $q->start_table({-width=>'100%'},{-border=>'0'});
+	print $q->Tr($q->td({align=>CENTER}, $q->img({src=>'/osap/logo.png'})));
+	print $q->end_table, "\n";
 	print $q->start_form;
-	print $q->h2({align=>CENTER}, "Disconnected.");
-	print $q->p("&nbsp;Thank you for using the LAAS Network.");
-	print $q->p("&nbsp;You are now disconnected.");
+	print $q->h2({align=>CENTER}, "D&eacute;connexion r&eacute;ussie.");
+	print $q->p("Merci d'utiliser le r&eacute;seau Datlan.");
 	print $q->start_table({width=>'100%'});
 	print $q->Tr({align=>CENTER},
-		     $q->td($q->submit({-name=>'OK', -value=>'Reconnect',
+		     $q->td($q->submit({-name=>'OK', -value=>'Reconnexion',
 					-class=>'oswap-button-ok'})));
 	print $q->end_table;
 	print $q->end_form;
 }
 
-#----------------------------------------------------------------------
-#
-# Check e-mail address 
-#
-sub valid_email
+sub login_mysql
 {
-	my $addr = $_[0];
-	
-	if ($addr =~ /^(\w|\-|\_|\.|\+)+\@(((\w|\-|\_)+\.)+[a-zA-Z]{2,})$/) {
-		$domain = "$2";
-		my $query = $resolver->search($domain);
-		if ($query) {
-			return 1;
-		} else {
-			my @mx = mx($resolver, $domain);
-			if (@mx) {
-				return 1;
-			} else {
-				syslog(LOG_INFO, "valid_email \'$addr\': can't find RR for $domain");
-				return 0;
-			}
-		}
-	} else {
-		return 0;
-	}
+	my $name = $_[0];
+	my $passwd = $_[1];
+
+	$dbh = DBI->connect('DBI:mysql:database=datlan;host=wayt.me', 'gw', 'toto42') || die "Could not connect to database: $DBI::errstr";
+
+	$sql = "SELECT SHA1(?) = use_password FROM t_user WHERE use_username = ?";
+	$sth = $dbh->prepare($sql);
+ 	$sth->execute($passwd, $name) || die "SQL Error: $DBI::errstr\n";
+
+    my ($check) = $sth->fetchrow_array;
+    $sth->finish();
+	$dbh->disconnect();
+    if ($check eq "1") {
+        return 1;
+    }
+    return 0;
 }
 
 #----------------------------------------------------------------------
@@ -149,22 +136,22 @@ sub validate {
 	my $q = $_[0];
 	
 	my $name = $q->param('name');
-	my $email = $q->param('email');
+	my $pass = $q->param('password');
 	my $terms = $q->param('terms');
 	my $ip = $q->remote_addr;
 
-	syslog(LOG_DEBUG, "validate \'$name\', \'$email\', $ip");
+	syslog(LOG_DEBUG, "validate \'$name\', \'$pass\', $ip");
 	if ($name eq "") {
-		return "Please fill the \'Name\' field.";
+		return "Merci de renseigner votre nom d'utilisateur.";
 	}
-	if ($email eq "") {
-		return "Please fill the \'E-mail\' field.";
-	}
-	if (!valid_email($email)) {
-		return "Please use a valid \'E-mail\' address.";
+	if ($pass eq "") {
+		return "Merci de renseigner le mot de passe.";
 	}
 	if (!defined($terms) || $terms ne 'on') {
-		return "Please accept the term of services.";
+		return "Merci de valider les conditions d'utilisations.";
+	}
+	if (!login_mysql($name, $pass)) {
+		return "Mauvaise combinaison Utilisateur/Mot de passe.";
 	}
 	return "";
 }
@@ -197,9 +184,7 @@ sub osapd_client {
 sub add_user {
 	my $q = $_[0];
 	my $ip = $q->remote_addr;
-	my $email = $q->param('email');
-	my $name = $q->param('name');
-	my $user = "$name/$email";
+	my $user = $q->param('name');
 	$user =~ s/\s+/_/g;
 	$user = escapeHTML($user);
 
@@ -244,9 +229,9 @@ $resolver = Net::DNS::Resolver->new;
 
 if ($q->request_method eq "GET") {
 	# First time?
-	my $email = test_user($q);
-	if ($email ne "") {
-		$q->param(-name=>'email', -value=>"$email");
+	my $user = test_user($q);
+	if ($user ne "") {
+		$q->param(-name=>'name', -value=>"$user");
 		session_window($q);
 	} else {
 		login_window($q);
